c66f16d30acd4ded046741ae69cf804cbea20fd6
Josa Gesell <josa@gesell.me>
2015-10-24 15:27:33 +0200

    Add some specs for actionscript

--- spec/languages/actionscript-spec.js
+++ spec/languages/actionscript-spec.js
@@ -14,18 +14,49 @@ describe('ParserActionScript', () => {
   })
 
   describe('parse_function()', () => {
-    it('should be implemented', () => expect(false).toBe(true))
+
+    it('should parse anonymous function', () => {
+      const out = parser.parse_function('function (a:String)')
+      expect(out).toEqual([null, 'a:String', null, {} ])
+    })
+
+    it('should parse function', () => {
+      const out = parser.parse_function('name : function(a:String)')
+
+      // expect(out).toEqual([ 'name', 'a:String', null, {} ])
+      expect(out).toEqual([ null, 'a:String', null, {} ])
+    })
+
+    it('should parse function', () => {
+      const out = parser.parse_function('function name(a:String)')
+
+      // expect(out).toEqual([ 'name', 'a:String', null, {} ])
+      expect(out).toEqual([ null, 'a:String', null, {} ])
+    })
+
+    it('should parse function', () => {
+      const out = parser.parse_function('name = function(a:String)')
+
+      // expect(out).toEqual([ 'name', 'a:String', null, {} ])
+      expect(out).toEqual([ null, 'a:String', null, {} ])
+    })
   })
 
   describe('parse_var()', () => {
-    it('should be implemented', () => expect(false).toBe(true))
+    it('should always return null', () => {
+      const out = parser.parse_var('foo = "Bar"')
+      expect(out).toEqual(null)
+    })
   })
 
   describe('get_arg_name()', () => {
-    it('should be implemented', () => expect(false).toBe(true))
+    xit('should be implemented', () => expect(false).toBe(true))
   })
 
   describe('get_arg_type()', () => {
-    it('should be implemented', () => expect(false).toBe(true))
+    it('should always return null', () => {
+      const out = parser.parse_var('')
+      expect(out).toEqual(null)
+    })
   })
 })
